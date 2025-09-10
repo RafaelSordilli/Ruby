f79af0cfd43ca823d9c5549fac980de78caf96d0
rafaelsordilli <rafaelsordilli@gmail.com>
2025-09-10 14:34:08 -0300

    first commit

---
+++ biblioteca.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:"
+nome = gets.chomp
+
+print "Digite seu sobrenome:"
+sobrenome = gets.chomp
+
+print "Digite sua idade:"
+idade = gets.chomp.to_i
+
+puts "O(a) #{nome} #{sobrenome} tem #{idade} anos de idade!"
